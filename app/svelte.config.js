bab3ace7a02fd15a26a81f196ddfa680c539f72e
stephane-klein <contact@stephane-klein.info>
2024-09-12 19:49:21 +0200

    Remove csrf.checkOrigin svelte config

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -2,10 +2,7 @@ import adapter from '@sveltejs/adapter-node';
 
 const config = {
     kit: {
-        adapter: adapter(),
-        csrf: {
-            checkOrigin: false
-        }
+        adapter: adapter()
     }
 };
 
