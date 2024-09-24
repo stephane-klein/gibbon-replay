9e2dc41eeceebe5ff654ae5254fd01b33841e525
stephane-klein <contact@stephane-klein.info>
2024-09-24 23:47:30 +0200

    Add external page to test beforeunload call

--- demosite/src/routes/+page.svelte
+++ demosite/src/routes/+page.svelte
@@ -2,4 +2,5 @@
     <li><a href="page1/">Page 1</a></li>
     <li><a href="page2/">Page 2</a></li>
     <li><a href="page3/">Page 3</a></li>
+    <li><a href="https://example.com/">example.com</a></li>
 </ul>
