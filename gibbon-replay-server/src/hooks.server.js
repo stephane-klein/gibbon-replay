6203e4b2020a91d53088eb9a18a87ed692deba7a
stephane-klein <contact@stephane-klein.info>
2025-05-30 21:20:00 +0200

    Automatically delete sessions after X days
    The number of days is defined by the environment variable KEEP_SESSION_AGE_DAYS

--- gibbon-replay-server/src/hooks.server.js
+++ gibbon-replay-server/src/hooks.server.js
@@ -1,7 +1,42 @@
-import { migrate } from './lib/server/db.js';
+import cron from 'node-cron';
+import addDays from 'date-fns/addDays';
+import db, { migrate } from './lib/server/db.js';
+
+function deleteOldSessions(keep_session_age_days) {
+    const delete_sessions_older_than_this_date = addDays(
+        new Date(),
+        -keep_session_age_days
+    ).toISOString();
+    const sessions_to_delete = db().queryFirstCell(
+        'SELECT COUNT(*) FROM sessions WHERE timestamp < datetime(?)',
+        delete_sessions_older_than_this_date
+    );
+    if (sessions_to_delete == 0) {
+        console.log(`No sessions older than ${delete_sessions_older_than_this_date} (${keep_session_age_days} days) to delete`);
+    } else {
+        console.log(
+            `Starting deletion of ${sessions_to_delete} sessions older than ${delete_sessions_older_than_this_date} (${keep_session_age_days} days)...`
+        );
+        db().prepare(`
+            DELETE FROM sessions
+            WHERE
+                timestamp < datetime(?)
+        `).run(delete_sessions_older_than_this_date);
+        console.log(
+            `${sessions_to_delete} deleted with success`
+        );
+    }
+}
 
 export async function init() {
     migrate();
+
+    cron.schedule(
+        '0 0 4 * *',
+        async() => {
+            deleteOldSessions(process.env.KEEP_SESSION_AGE_DAYS || 250);
+        }
+    );
 }
 
 export async function handle({ event, resolve }) {
