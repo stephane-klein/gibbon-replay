8d8af8c56e5958a1de9ec0ca0cb7fd1edb067023
stephane-klein <contact@stephane-klein.info>
2025-06-08 13:59:06 +0200

    WIP

--- gibbon-replay-server/src/compute_all_sessions_has_mouse_movement.js
+++ gibbon-replay-server/src/compute_all_sessions_has_mouse_movement.js
@@ -1,7 +1,39 @@
+import { EventType, IncrementalSource } from 'rrweb';
 import db, { migrate } from './lib/server/db.js';
 
 migrate();
 
+db().run('UPDATE sessions SET has_mouse_movement=FALSE');
+
+let numberSessionHaveMouseMouvement = 0;
+let totalSession = 0;
 db().query('SELECT session_uuid FROM sessions').forEach(({session_uuid}) => {
-    console.log(session_uuid);
+    totalSession++;
+    db().query(
+        'SELECT data FROM session_events WHERE session_uuid=?',
+        session_uuid
+    ).some(({data}) => {
+        if (
+            JSON.parse(data).some(
+                (event) => (
+                    (event.type === EventType.IncrementalSnapshot) && 
+                    (event.data.source === IncrementalSource.MouseMove)
+                )
+            )
+        ) {
+            numberSessionHaveMouseMouvement++;
+            db().run(
+                `
+                    UPDATE sessions 
+                       SET has_mouse_movement=TRUE
+                     WHERE session_uuid=?
+                `,
+                session_uuid
+            );
+            return true;
+        }
+        return false;
+    });
 });
+
+console.log(`${numberSessionHaveMouseMouvement}/${totalSession} session have mouve movement`);
